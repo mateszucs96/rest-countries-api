b8228cd6134a7830df8533852e725ab40c8111bf
szucsmate <szucsmate096@gmail.com>
2022-05-16 21:16:30 +0200

    add logic to display all countries and selected countries by region

--- src/app.js
+++ src/app.js
@@ -1,57 +1,102 @@
-const fetchData = async (name) => {
-    const res = await fetch(`https://restcountries.com/v2/name/${name}`);
-    const dataArr = await res.json();
-    const data = dataArr[0];
+const fetchAllCountries = async () => {
+    const res = await fetch(`https://restcountries.com/v3.1/all`);
+    const data = await res.json();
     return data;
 }
 
-fetchData('hungary')
+const fetchByRegion = async (region) => {
+    const res = await fetch(`https://restcountries.com/v3.1/region/${region}`)
+    const data = await res.json();
 
-{/* <div class="card">
-        <div class="card__flag">
-            <img src="./assets/Flag_of_Germany.svg.png" alt="german-flag" class="flag">
-        </div>
-        <div class="card__info">
-            <h1 class="card__title">
-                Germany
-            </h1>
-            <div class="card__details">
-                <p class="info-label">Population: <span class="info-data population">84 000 000</span></p>
-                <p class="info-label">Region: <span class="info-data region">Europe</span></p>
-                <p class="info-label">Capital: <span class="info-data capital">Berlin</span></p>
-            </div>
-        </div>
-    </div> */}
+    return data;
+}
+
+
+const fetchBySearch = async (name) => {
+    const res = await fetch(`https://restcountries.com/v3.1/name/${name}`)
+    const data = await res.json();
+    return data;
+}
 
 
 class App {
-    flag = document.querySelector('.flag');
-    countryName = document.querySelector('.card__title');
-    population = document.querySelector('.population');
-    region = document.querySelector('.region');
-    capital = document.querySelector('.capital');
-    cards = document.querySelectorAll('.card');
+    cards = document.querySelector('.card-container');
+    selector = document.querySelector('.countries');
+    options = document.querySelectorAll('option');
 
     constructor() {
+        this.displayCountry();
+        this.selector.addEventListener('change', this.showSelect.bind(this))
+    }
+
+    showSelect() {
+        const selectedCountry = this.selector.options[this.selector.selectedIndex].value
+
+        fetchByRegion(selectedCountry).then(res => res.forEach(data => {
+            const parts = data.population.toString().split(".");
+            parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            const num = parts.join(".");
+            console.log(data)
+
+            const html = `
+                <div class="card" >
+                    <div class="card__flag">
+                        <img src="${data.flags.svg}">
+                    </div>
+                    <div class="card__info">
+                        <h1 class="card__title">
+                            ${data.name.common}
+                        </h1>
+                        <div class="card__details">
+                            <p class="info-label">Population: <span class="info-data">${num}</span></p>
+                            <p class="info-label">Region: <span class="info-data">${data.region}</span></p>
+                            <p class="info-label">Capital: <span class="info-data">${data.capital}</span></p>
+                        </div>
+                    </div>
+                </div >
+                `
+            this.cards.insertAdjacentHTML('beforeend', html)
+        }))
+
+
+
+        this.cards.textContent = '';
 
     }
 
-    async displayCountry() {
+    displayCountry() {
 
-        const data = await fetchData('hungary');
-        this.flag.src = data.flag
-        this.countryName.textContent = data.name;
-        //modify number to separate with comma
-        const parts = data.population.toString().split(".");
-        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        const num = parts.join(".");
+        fetchAllCountries().then(res => res.forEach(data => {
 
-        this.population.textContent = num;
-        this.region.textContent = data.region;
-        this.capital.textContent = data.capital;
-        console.log()
+            const parts = data.population.toString().split(".");
+            parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            const num = parts.join(".");
+
+            const html = `
+                <div class="card" >
+                    <div class="card__flag">
+                        <img src="${data.flags.svg}">
+                    </div>
+                    <div class="card__info">
+                        <h1 class="card__title">
+                            ${data.name.common}
+                        </h1>
+                        <div class="card__details">
+                            <p class="info-label">Population: <span class="info-data">${num}</span></p>
+                            <p class="info-label">Region: <span class="info-data">${data.region}</span></p>
+                            <p class="info-label">Capital: <span class="info-data">${data.capital}</span></p>
+                        </div>
+                    </div>
+                </div >
+                `
+            this.cards.insertAdjacentHTML('beforeend', html)
+
+        }));
+    }
+
+    displaySearch() {
+
     }
 }
 
-const app = new App()
-app.displayCountry()
+const app = new App();
