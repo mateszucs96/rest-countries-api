7ec7a276e00a0007a0ddf44aae2d286b004f9e40
szucsmate <szucsmate096@gmail.com>
2022-05-17 00:49:17 +0200

    add logic to search field

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+let countryNames = [];
+// let value;
+
 const fetchAllCountries = async () => {
     const res = await fetch(`https://restcountries.com/v3.1/all`);
     const data = await res.json();
@@ -18,15 +21,27 @@ const fetchBySearch = async (name) => {
     return data;
 }
 
+const debounce = (cb, delay = 500) => {
+    return (...args) => {
+        setTimeout(() => {
+            cb(...args)
+        }, delay);
+    }
+}
+
 
 class App {
+
     cards = document.querySelector('.card-container');
     selector = document.querySelector('.countries');
     options = document.querySelectorAll('option');
+    inputForm = document.querySelector('.input-form');
+    input = document.querySelector('.input');
 
     constructor() {
         this.displayCountry();
         this.selector.addEventListener('change', this.showSelect.bind(this))
+        this.inputForm.addEventListener('keyup', this.displaySearch.bind(this))
     }
 
     showSelect() {
@@ -65,9 +80,10 @@ class App {
     }
 
     displayCountry() {
-
+        this.cards.textContent = '';
+        countryNames = [];
         fetchAllCountries().then(res => res.forEach(data => {
-
+            countryNames.push(data.name.common.toLowerCase())
             const parts = data.population.toString().split(".");
             parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
             const num = parts.join(".");
@@ -92,10 +108,64 @@ class App {
             this.cards.insertAdjacentHTML('beforeend', html)
 
         }));
+
     }
+    async displaySearch(e) {
+        e.preventDefault();
+        const value = this.input.value.toLowerCase()
 
-    displaySearch() {
+        // const updateDebounceText = debounce(text => {
+        //     console.log(text)
+        //     value = text;
+        //     console.log(value)
+        // })
 
+        // updateDebounceText(this.input.value.toLowerCase())
+
+        const filteredCountries = countryNames.filter(country => {
+            return country.startsWith(value)
+        })
+        if (value) {
+
+            console.log(filteredCountries)
+
+
+
+            this.cards.textContent = '';
+            filteredCountries.forEach(el => fetchBySearch(el).then(res => res.forEach(data => {
+                const parts = data.population.toString().split(".");
+                parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                const num = parts.join(".");
+
+                const html = `
+                <div class="card" >
+                    <div class="card__flag">
+                        <img src="${data.flags.svg}">
+                    </div>
+                    <div class="card__info">
+                        <h1 class="card__title">
+                            ${data.name.common}
+                        </h1>
+                        <div class="card__details">
+                            <p class="info-label">Population: <span class="info-data">${num}</span></p>
+                            <p class="info-label">Region: <span class="info-data">${data.region}</span></p>
+                            <p class="info-label">Capital: <span class="info-data">${data.capital}</span></p>
+                        </div>
+                    </div>
+                </div >
+                `
+                this.cards.insertAdjacentHTML('beforeend', html)
+
+            })))
+        }
+        else {
+
+            this.displayCountry();
+        }
+
+
+
+        // this.input.value = ''
     }
 }
 
