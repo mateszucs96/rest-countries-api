73213007b07071d8d0e51bf6fabb5a7ab0f20f8a
mateszucs96 <szucsmate096@gmail.com>
2023-01-11 00:23:42 +0100

    fix stlying

--- src/controller.js
+++ src/controller.js
@@ -1,62 +1,61 @@
-import * as model from './model.js';
-import Country from './view.js';
+import * as model from "./model.js";
+import Country from "./view.js";
 
+const controlSearch = (e) => {
+  e.preventDefault();
+  model.loadSearch(e.target.value.toLowerCase());
 
-const controlSearch = e => {
-    e.preventDefault();
-    model.loadSearch(e.target.value.toLowerCase())
-
-    model.state.searched.forEach(country => {
-        Country.renderCard(country)
-    });
-}
+  model.state.searched.forEach((country) => {
+    Country.renderCard(country);
+  });
+};
 
 const controlSelect = (value) => {
-    console.log(value)
-    if (value === 'Filter by Region') return;
-    model.state.filtered = model.state.countries.filter(el => el.region.toLowerCase() === value.toLowerCase());
-    model.state.filtered.forEach(el => Country.renderCard(el));
-}
+  console.log(value);
+  if (value === "Filter by Region") return;
+  model.state.filtered = model.state.countries.filter(
+    (el) => el.region.toLowerCase() === value.toLowerCase()
+  );
+  model.state.filtered.forEach((el) => Country.renderCard(el));
+};
 
 const controlCountries = async () => {
-    await model.fetchData('https://restcountries.com/v3.1/all')
-    model.state.countries.forEach(el => {
-        Country.renderCard(el);
-    })
-    Country.addHandlerDetails(controlDetails);
-}
+  await model.fetchData("https://restcountries.com/v3.1/all");
+  model.state.countries.forEach((el) => {
+    Country.renderCard(el);
+  });
+  Country.addHandlerDetails(controlDetails);
+};
 
 const controlDetails = (countryName) => {
-
-    model.loadDetails(countryName, model.state.details.nativeName)
-    Country.renderDetails(
-        model.state.details.result,
-        model.state.details.nativeName,
-        model.state.details.currencies,
-        model.state.details.languages,
-        model.state.details.borderData,
-    );
-    Country.addHandlerBackButton(controlCountries)
-    Country.addHandlerButton(contorlBorders)
-}
+  model.loadDetails(countryName, model.state.details.nativeName);
+  Country.renderDetails(
+    model.state.details.result,
+    model.state.details.nativeName,
+    model.state.details.currencies,
+    model.state.details.languages,
+    model.state.details.borderData
+  );
+  Country.addHandlerBackButton(controlCountries);
+  Country.addHandlerButton(contorlBorders);
+};
 
 const contorlBorders = (countryName) => {
-    model.loadDetails(countryName, model.state.details.nativeName)
-    Country.renderDetails(
-        model.state.details.result,
-        model.state.details.nativeName,
-        model.state.details.currencies,
-        model.state.details.languages,
-        model.state.details.borderData,
-    );
-    Country.addHandlerBackButton(controlCountries)
-    Country.addHandlerButton(contorlBorders)
-}
+  model.loadDetails(countryName, model.state.details.nativeName);
+  Country.renderDetails(
+    model.state.details.result,
+    model.state.details.nativeName,
+    model.state.details.currencies,
+    model.state.details.languages,
+    model.state.details.borderData
+  );
+  Country.addHandlerBackButton(controlCountries);
+  Country.addHandlerButton(contorlBorders);
+};
 
 const init = () => {
-    controlCountries();
-    Country.addHandlerSelect(controlSelect);
-    Country.addHandlerSearch(controlSearch);
-
-}
+  controlCountries();
+  Country.addHandlerSelect(controlSelect);
+  Country.addHandlerSearch(controlSearch);
+};
 init();
--- src/view.js
+++ src/view.js
@@ -1,60 +1,63 @@
 class Country {
-    data;
-    card;
+  data;
+  card;
 
-    cards = document.querySelector('.card-container');
-    selector = document.querySelector('.countries');
-    inputForm = document.querySelector('.input-form');
-    detailsSection = document.querySelector('.details-section');
+  cards = document.querySelector(".card-container");
+  selector = document.querySelector(".countries");
+  inputForm = document.querySelector(".input-form");
+  detailsSection = document.querySelector(".details-section");
 
-    addHandlerSelect(handler) {
-        this.selector.addEventListener('click', () => {
-            this.cards.textContent = '';
-            handler(this.selector.options[this.selector.selectedIndex].value)
-        });
-    };
+  addHandlerSelect(handler) {
+    this.selector.addEventListener("click", () => {
+      this.cards.textContent = "";
+      handler(this.selector.options[this.selector.selectedIndex].value);
+    });
+  }
 
-    addHandlerSearch(handler) {
-        this.inputForm.addEventListener('input', (e) => {
-            this.cards.textContent = '';
-            handler(e)
-        });
-    };
+  addHandlerSearch(handler) {
+    this.inputForm.addEventListener("input", (e) => {
+      this.cards.textContent = "";
+      handler(e);
+    });
+  }
 
-    addHandlerDetails(handler) {
-        this.card.forEach(el => el.addEventListener('click', e => {
-            const clicked = e.target.closest('.card')
-            const countryName = clicked.children[1].children[0].textContent.trim()
-            handler(countryName)
-        }))
-    };
+  addHandlerDetails(handler) {
+    this.card.forEach((el) =>
+      el.addEventListener("click", (e) => {
+        const clicked = e.target.closest(".card");
+        const countryName = clicked.children[1].children[0].textContent.trim();
+        handler(countryName);
+      })
+    );
+  }
 
-    addHandlerButton(handler) {
-        document.querySelectorAll('.border-button').forEach(el => el.addEventListener('click', (e) => {
-            const clicked = e.target.textContent
-            handler(clicked)
-        }))
-    }
+  addHandlerButton(handler) {
+    document.querySelectorAll(".border-button").forEach((el) =>
+      el.addEventListener("click", (e) => {
+        const clicked = e.target.textContent;
+        handler(clicked);
+      })
+    );
+  }
 
-    addHandlerBackButton(handler) {
-        document.querySelector('.back-button').addEventListener('click', () => {
-            console.log('back')
-            handler()
-        })
+  addHandlerBackButton(handler) {
+    document.querySelector(".back-button").addEventListener("click", () => {
+      console.log("back");
+      handler();
+    });
+  }
 
-    }
-
-    renderBorderButton(text) {
-        return `        
+  renderBorderButton(text) {
+    return `        
         <button class="btn border-button">${text}</button>
-        `
-    }
+        `;
+  }
 
-    renderDetails(data, nativeName, currencies, languages, borders, handler) {
-        this.data = data[0]
-        console.log(this.data)
-        this.cards.textContent = '';
-        const html = `
+  renderDetails(data, nativeName, currencies, languages, borders, handler) {
+    this.data = data[0];
+    console.log(this.data);
+    this.cards.textContent = "";
+    const html = `
         <button class="btn back-button">Back</button>
 
         <div class="country">
@@ -67,13 +70,23 @@ class Country {
             </h1>
             <div class="country__details">
                 <p class="country-label">Native name: <span class="country-data population">${nativeName}</span></p>
-                <p class="country-label">Population: <span class="country-data population">${this.data.population}</span></p>
-                <p class="country-label">Region: <span class="country-data region">${this.data.region}</span></p>
-                <p class="country-label">Sub Region: <span class="country-data capital">${this.data.subregion}</span></p>
-                <p class="country-label">Capital: <span class="country-data capital">${this.data.capital}</span></p>
+                <p class="country-label">Population: <span class="country-data population">${
+                  this.data.population
+                }</span></p>
+                <p class="country-label">Region: <span class="country-data region">${
+                  this.data.region
+                }</span></p>
+                <p class="country-label">Sub Region: <span class="country-data capital">${
+                  this.data.subregion
+                }</span></p>
+                <p class="country-label">Capital: <span class="country-data capital">${
+                  this.data.capital
+                }</span></p>
             </div>
             <div class="country__secondary-details">
-                <p class="country-label">Top Level Domain: <span class="country-data capital">${this.data.tld[0]}</span></p>
+                <p class="country-label">Top Level Domain: <span class="country-data capital">${
+                  this.data.tld[0]
+                }</span></p>
 
                 <p class="country-label">Currencies: <span class="country-data region">${currencies}</span></p>
                 <p class="country-label">Languages: <span class="country-data region">${languages}</span></p>                
@@ -83,21 +96,24 @@ class Country {
                 Boder Countries
             </h4>
             <div class="borders">
-                ${!this.data.borders
-                ? `sorry no borders`
-                : borders.map(el => this.renderBorderButton(el.name.common)).join('')}               
+                ${
+                  !this.data.borders
+                    ? `sorry no borders`
+                    : borders
+                        .map((el) => this.renderBorderButton(el.name.common))
+                        .join("")
+                }               
             </div>
         </div>
-        `
+        `;
 
-        this.detailsSection.textContent = '';
-        this.detailsSection.insertAdjacentHTML('beforeend', html);
-    };
+    this.detailsSection.textContent = "";
+    this.detailsSection.insertAdjacentHTML("beforeend", html);
+  }
 
-    renderCard(data) {
-
-        this.data = data
-        const html = `
+  renderCard(data) {
+    this.data = data;
+    const html = `
         <div class="card" >
             <div class="card__flag">
                 <img src="${this.data.flags.svg}">
@@ -113,10 +129,10 @@ class Country {
                 </div>
             </div>
         </div >
-        `
-        this.cards.insertAdjacentHTML('beforeend', html);
-        this.card = document.querySelectorAll('.card');
-    };
-};
+        `;
+    this.cards.insertAdjacentHTML("beforeend", html);
+    this.card = document.querySelectorAll(".card");
+  }
+}
 
 export default new Country();
